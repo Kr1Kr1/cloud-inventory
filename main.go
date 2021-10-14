a80a2367f253b19dce2b0a35e70c5aa664473bca
Karin <karin@api.video>
2021-10-14 12:28:42 +0200

    (feat) Add start of ovh billing estimate

--- main.go
+++ main.go
@@ -14,6 +14,45 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// All prices are per month and HT
+var prices = map[string]string{
+	"b2-7":      "22",
+	"b2-15":     "42",
+	"b2-30":     "85",
+	"b2-60":     "165",
+	"b2-120":    "325",
+	"c2-7":      "32",
+	"c2-15":     "62",
+	"c2-30":     "125",
+	"c2-60":     "245",
+	"c2-120":    "485",
+	"d2-2":      "5",
+	"d2-4":      "10",
+	"d2-8":      "18",
+	"i1-45":     "200",
+	"i1-90":     "400",
+	"i1-180":    "800",
+	"r2-15":     "32",
+	"r2-30":     "37",
+	"r2-60":     "72",
+	"r2-120":    "145",
+	"r2-240":    "285",
+	"t1-45":     "799",
+	"t1-90":     "1599",
+	"t1-180":    "3199",
+	"t2-45":     "825",
+	"t2-90":     "1649",
+	"t2-180":    "3299",
+	"advance-5": "190",
+	"fs-72t":    "390",
+	"infra-1":   "95",
+	"infra-4":   "219",
+	"infra-3":   "209",
+	"hgr-sds-2": "576",
+	"fs-48t":    "314",
+	"advance-3": "81",
+}
+
 // PrettyPrint Prettify the display
 func PrettyPrint(v interface{}) (err error) {
 	b, err := json.MarshalIndent(v, "", "  ")
@@ -96,9 +135,17 @@ func ovhListing() {
 
 	// Pretty Print
 	w := tabwriter.NewWriter(os.Stdout, 20, 1, 3, ' ', 0)
-	fmt.Fprintln(w, "CLOUD TYPE\tREGION\tNAME\tIP\tSTATE")
+	fmt.Fprintln(w, "CLOUD TYPE\tREGION\tNAME\tIP\tSTATE\tBILLING (Month, HT)")
 	for _, instance := range listOfInstances {
-		w.Write([]byte(fmt.Sprintf("%s\t%s\t%s\t%s\t%s\n", "Public Cloud", instance.Region, instance.Name, instance.IPAddresses[0].IP, instance.Status)))
+		// Get monthly price
+		var price string
+		billingName := strings.Split(instance.PlanCode, ".")
+		if val, ok := prices[strings.ToLower(billingName[0])]; ok {
+			price = val
+		} else {
+			price = instance.PlanCode
+		}
+		w.Write([]byte(fmt.Sprintf("%s\t%s\t%s\t%s\t%s\t%s\n", "Public Cloud", instance.Region, instance.Name, instance.IPAddresses[0].IP, instance.Status, price)))
 	}
 	// w.Flush()
 
@@ -115,7 +162,14 @@ func ovhListing() {
 	// w = tabwriter.NewWriter(os.Stdout, 20, 1, 3, ' ', 0)
 	// fmt.Fprintln(w, "CLOUD TYPE\tREGION\tNAME")
 	for _, server := range listOfDedicatedServers {
-		w.Write([]byte(fmt.Sprintf("%s\t%s\t%s\t%s\t%s\n", "Bare Metal Cloud", strings.ToUpper(server.Datacenter), server.Reverse, server.IP, strings.ToUpper(server.State))))
+		// Get monthly price
+		var price string
+		if val, ok := prices[strings.ToLower(server.CommercialRange)]; ok {
+			price = val
+		} else {
+			price = server.CommercialRange
+		}
+		w.Write([]byte(fmt.Sprintf("%s\t%s\t%s\t%s\t%s\t%s\n", "Bare Metal Cloud", strings.ToUpper(server.Datacenter), server.Reverse, server.IP, strings.ToUpper(server.State), price)))
 	}
 	w.Flush()
 }
